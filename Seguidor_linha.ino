fc93ce40209a4176ddcd1e8ca6fd2d4d32d59a02
andre23arruda <33252657+andre23arruda@users.noreply.github.com>
2017-12-22 03:07:27 -0200

    Update Seguidor_linha.ino

--- Seguidor_linha.ino
+++ Seguidor_linha.ino
@@ -7,11 +7,9 @@
 
 float error = 0, P = 0, I = 0, D = 0, PID_value = 0; // Iniciando o PID
 float previous_error = 0, previous_I = 0;
-int a[6] = {0, 0, 0, 0, 0,0}; // Leiura analogica dos sensores
-int media[6] = {0, 0, 0, 0, 0,0}; // Media da leitura dos sensores
+
 int sensor[6] = {0, 0, 0, 0, 0,0}; // Leitura "digital" dos sensores
-int alto[6] = {0, 0, 0, 0, 0,0}; 
-int baixo[6] = {1000, 1000, 1000, 1000, 1000,1000};
+
 int past_sensor5=0; // variavel auxiliar para contar faixa direita
 int faixas=0; // contador de faixas direita
 int contador=0; // variavel auxiliar para condição de parada
@@ -52,60 +50,12 @@ void loop() // Todas as funções rodam dentro do loop
 
 void calcular_media()
 {
-  a[0] = analogRead(A5);
-  a[1] = analogRead(A4);
-  a[2] = analogRead(A3);
-  a[3] = analogRead(A2);
-  a[4] = analogRead(A1);
-  a[5] = analogRead(A0); //sensor direita
-
-
-  if (a[0] > alto[0])
-      alto[0] = a[0];
-    if(a[0] < baixo[0])
-      baixo[0] = a[0];
-    media[0] = (alto[0]+baixo[0])/2;
-    
-    if (a[1] > alto[1])
-      alto[1] = a[1];
-    if(a[1] < baixo[1])
-      baixo[1] = a[1];
-    media[1] = (alto[1]+baixo[1])/2;
-  
-   if (a[2] > alto[2])
-      alto[2] = a[2];
-    if(a[2] < baixo[2])
-      baixo[2] = a[2];
-    media[2] = (alto[2]+baixo[2])/2;
-    
-    if (a[3] > alto[3])
-      alto[3] = a[3];
-    if(a[3] < baixo[3])
-      baixo[3] = a[3];
-    media[3] = (alto[3]+baixo[3])/2;
-    
-    if (a[4] > alto[4])
-      alto[4] = a[4];
-    if(a[4] < baixo[4])
-      baixo[4] = a[4];
-    media[4] = (alto[4]+baixo[4])/2;
-
-      if (a[5] > alto[5])
-      alto[5] = a[5];
-    if(a[5] < baixo[5])
-      baixo[5] = a[5];
-    media[5] = (alto[5]+baixo[5])/2;
-}
-
-void read_sensor_values()
-{
-    sensor[0] = (a[0]<media[0])? 1 : 0;
-    sensor[1] = (a[1]<media[1])? 1 : 0;
-    sensor[2] = (a[2]<media[2])? 1 : 0;
-    sensor[3] = (a[3]<media[3])? 1 : 0;
-    sensor[4] = (a[4]<media[4])? 1 : 0;
-    sensor[5] = (a[5]<media[5])? 1 : 0;
-   
+  sensor[0] = digitalRead(A5);
+  sensor[1] = digitalRead(A4);
+  sensor[2] = digitalRead(A3);
+  sensor[3] = digitalRead(A2);
+  sensor[4] = digitalRead(A1);
+  sensor[5] = digitalRead(A0); //sensor direita   
 
 // Se quiser verificar a leitura dos sensores
 /*Serial.print(sensor[0]);
@@ -175,8 +125,8 @@ void motor_control()
   } 
   else if (tempo_atual>=tempo_calib)
   {
-    if (faixas== Faixas_Direita && contador==0){
-      tempo_final=tempo_atual+500;
+    if (faixas == Faixas_Direita && contador==0){
+      tempo_final=tempo_atual + 200;
       contador++;
     }
     if (tempo_atual>=tempo_final && contador>0)
@@ -188,7 +138,7 @@ void motor_control()
   }
 }
 
-void conta_faixas()
+void conta_faixas() // funcao para contar faixas da direita
 {
   if (past_sensor5==0 && sensor[5]==1 && tempo_atual>=tempo_calib){
     faixas=faixas+1;
